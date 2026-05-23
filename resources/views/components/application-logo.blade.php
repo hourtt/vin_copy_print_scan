7a93f392c7c2779410965bae20a695dd241f7735
hourt <bunhourt79@gmail.com>
2026-05-23 10:04:01 +0700

    [UPDATE] Replace application logo with conditional rendering for login and registration routes

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,9 @@
-<img src="{{ asset('storage/images/logo.png') }}" alt="logo" class="w-24 h-24 fill-current">
+@if (request()->routeIs('login', 'register'))
+    <img src="{{ asset('storage/images/logo-horizontal-layout.png') }}" 
+         alt="Application Logo" 
+         class="h-20 w-auto">
+@else
+    <img src="{{ asset('storage/images/logo-icon-only.png') }}" 
+         alt="Application Logo" 
+         class="h-10 w-20 object-contain">
+@endif
