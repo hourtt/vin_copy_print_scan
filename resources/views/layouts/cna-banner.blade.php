d320f2edfa2a8df6228dff3a5c8e5f145a4ef70d
hourt <bunhourt79@gmail.com>
2026-05-25 21:35:02 +0700

    [ADD] Create join banner for guest users to encourage account creation

---
+++ resources/views/layouts/cna-banner.blade.php
@@ -0,0 +1,14 @@
+@guest
+    <div class="join-banner">
+        <div class="join-banner-inner">
+            <div>
+                <h2>Ready to <em>shop?</em></h2>
+                <p>Create a free account to unlock purchasing, track your orders, and save your favourites.</p>
+            </div>
+            <div class="join-actions">
+                <a href="{{ route('register') }}" class="btn btn-gold">Create free account</a>
+                <a href="{{ route('login') }}" class="btn btn-outline-white">Sign in</a>
+            </div>
+        </div>
+    </div>
+@endguest
