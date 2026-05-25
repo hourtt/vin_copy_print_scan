2ba715c3d880d0b8056ab41bbffed5a0495d8ade
hourt <bunhourt79@gmail.com>
2026-05-25 21:33:09 +0700

    [UPDATE] Refactor DatabaseSeeder to group seeder calls for better organization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       $this->call(AdminSeeder::class);
+        $this->call([
+            AdminSeeder::class,
+            CategorySeeder::class,  // Must run before ProductSeeder (products reference categories)
+            ProductSeeder::class,
+            VoucherSeeder::class,
+        ]);
     }
 }
