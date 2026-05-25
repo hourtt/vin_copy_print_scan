524cb06e23acda5cbde74e01930c1ec437f08304
hourt <bunhourt79@gmail.com>
2026-05-25 21:32:54 +0700

    [ADD] Create CategorySeeder for seeding categories using factory

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //* Create 15 categories using the factory
+        Category::factory()->count(15)->create();
+    }
+}
