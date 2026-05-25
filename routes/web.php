4716c0e215bf6de132c78b92bc7ea2442cd0ddf3
hourt <bunhourt79@gmail.com>
2026-05-25 21:36:21 +0700

    [UPDATE] Refactor routes to use controller methods for dashboard and orders

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/', [ProductController::class, 'index'])->name('dashboard');
+
+Route::post('/orders', [OrdersController::class, 'store'])->middleware('auth')->name('orders.store');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
