4113aed2940dee2433b5bb479a7a0229408db48c
hourt <bunhourt79@gmail.com>
2026-05-25 21:31:25 +0700

    [ADD] Add products relationship to Voucher model

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -2,11 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Voucher extends Model
 {
-    protected $fillable =[
-        'code','discount_type','discount_value','usage_limit','used_count','expires_at','is_active'
+    use HasFactory;
+    protected $fillable = [
+        'code',
+        'discount_type',
+        'discount_value',
+        'usage_limit',
+        'used_count',
+        'expires_at',
+        'is_active'
     ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'voucher_products', 'voucher_id', 'product_id');
+    }
 }
