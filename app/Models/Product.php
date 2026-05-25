01e07f12a644bfbb95f7e9e43f51692b6e279c9f
hourt <bunhourt79@gmail.com>
2026-05-25 21:31:17 +0700

    [ADD] Enhance Product model with category and voucher relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory;
     protected $fillable = [
-        'category_id','name','model_year','description','price','stock','image'
+        'category_id',
+        'name',
+        'model_year',
+        'description',
+        'price',
+        'stock',
+        'image'
     ];
+    public function category()
+    {
+        //* one product belongs to one category
+        return $this->belongsTo(Category::class);
+    }
+
+    public function voucher()
+    {
+        return $this->belongsToMany(Voucher::class, 'voucher_products', 'product_id', 'voucher_id');
+    }
 }
