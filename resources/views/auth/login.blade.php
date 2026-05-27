ded5d005f23865903d9336e8137d6acc8a92282b
hourt <bunhourt79@gmail.com>
2026-05-27 21:23:11 +0700

    [UPDATE] Replace input components with floating labels for login and registration forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,19 +7,15 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-floating-input id="email" name="email" type="email" label="Email" :value="old('email')" required
+                autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-password-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-floating-input id="password" name="password" type="password" label="Password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -27,14 +23,16 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
@@ -45,11 +43,12 @@
         </div>
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-            <p class="text-sm text-gray-600 mr-2">
-                {{ __('New user?') }}
-            </p>
-                <a class="underline text-sm text-gray-600 hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2" href="{{ route('register') }}">
-                    {{ __('Register here') }}
+                <p class="text-sm text-gray-600 mr-2">
+                    {{ __('New User?') }}
+                </p>
+                <a class="underline text-sm text-gray-600 hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2"
+                    href="{{ route('register') }}">
+                    {{ __('Register Here') }}
                 </a>
             @endif
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,15 +5,13 @@
         {{-- Row 1: First Name & Last Name --}}
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div>
-                <x-input-label for="first_name" :value="__('First Name')" />
-                <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')"
-                    required autofocus autocomplete="first_name" />
+                <x-floating-input id="first_name" name="first_name" type="text" label="First Name" :value="old('first_name')"
+                    required autofocus />
                 <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
             </div>
 
             <div>
-                <x-input-label for="last_name" :value="__('Last Name')" />
-                <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')"
+                <x-floating-input id="last_name" name="last_name" type="text" label="Last Name" :value="old('last_name')"
                     required autocomplete="last_name" />
                 <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
             </div>
@@ -21,24 +19,21 @@
 
         {{-- Row 2: Email (full width) --}}
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autocomplete="username" />
+            <x-floating-input id="email" name="email" type="email" label="Email" :value="old('email')" required
+                autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         {{-- Row 3: Password & Confirm Password --}}
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
             <div>
-                <x-input-label for="password" :value="__('Password')" />
-                <x-password-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                <x-floating-input id="password" name="password" type="password" label="Password" required
                     autocomplete="new-password" />
             </div>
 
             <div>
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-                <x-password-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                    name="password_confirmation" required autocomplete="new-password" />
+                <x-floating-input id="password_confirmation" name="password_confirmation" type="password"
+                    label="Confirm Password" required autocomplete="new-password" />
             </div>
         </div>
         <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -63,10 +58,10 @@
         {{-- Row 6: Already registered link --}}
         @if (Route::has('login'))
             <div class="mt-4 text-center text-sm text-gray-600">
-                {{ __('Already registered?') }}
+                {{ __('Already Registered?') }}
                 <a class="underline hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2"
                     href="{{ route('login') }}">
-                    {{ __('Sign in') }}
+                    {{ __('Sign In') }}
                 </a>
             </div>
         @endif
