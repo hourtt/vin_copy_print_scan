7b79c3ecb9d96dfe403bce1e9dd009fe4fb4fd05
hourt <bunhourt79@gmail.com>
2026-06-14 11:13:33 +0700

    [UPDATE] Redesign the class of the ticker bar (Already replaced the keyframe and the animation in to the tailwind configuration file)

--- resources/views/components/ticker-bar.blade.php
+++ resources/views/components/ticker-bar.blade.php
@@ -1,22 +1,21 @@
-  {{-- ticker bar --}}
-    <div class="ticker-bar" aria-hidden="true">
-        <div class="ticker-track">
-            @php
+{{-- ticker bar --}}
+<div class="overflow-hidden whitespace-nowrap w-full" aria-hidden="true">
+    <div class="inline-flex w-max animate-ticker-scroll">
+        @php
             $ticks = [
                 'Quality printing',
                 'A3 · A4 formats',
                 'Best Service',
-                'Color &amp; black-and-white',
+                'Color &amp; black & white',
                 'Scan to PDF or email',
                 'Pick-up or delivery',
                 'Trusted by more than 500 customers',
             ];
-            @endphp
-            @foreach (array_merge($ticks, $ticks) as $tick)
-                <span class="ticker-item">
-                    <span class="font font-bold"></span>
-                    {!! $tick !!}
-                </span>
-            @endforeach
-        </div>
+        @endphp
+        @foreach (array_merge($ticks, $ticks) as $tick)
+            <span class="inline-flex items-center px-8 shrink-0">
+                {!! $tick !!}
+            </span>
+        @endforeach
     </div>
+</div>
